22df77fc7dc9a8b67c38bcd22e235882bee25814
Saad Sarwar <saad.sarwar@heuristicsol.com>
2022-05-18 20:05:27 +0500

    ui-pages

--- client/src/pages/auth/VerificationCode.jsx
+++ client/src/pages/auth/VerificationCode.jsx
@@ -1,10 +1,47 @@
 import React from 'react';
+import { Input,
+  Checkbox,
+  Button,
+  Form,
+  Header,
+  Container,
+TextArea} from 'semantic-ui-react';
 import Auth from './Auth';
+import { Link } from 'react-router-dom';
 
 const VerificationCode = () => {
+  const handleSubmit =() => {
+    console.log("s")
+  }
   return (
+
     <Auth>
-      <h2>Verification Code page</h2>
+      
+      
+      <Container>
+      <p className='text-center'>Please Check you Email</p>
+      <p className='text-center'>We sent the verification code to your <br/> email@domain.com.<br/> 
+Please enter the code below</p>
+      
+        <Form onSubmit={handleSubmit}>
+          {/* <Form.Field>
+            <label>Email address</label>
+            <input placeholder='Enter your email' />
+          </Form.Field> */}
+          <Form.Field>
+            <label className='d-inline-block'>Verification code</label>
+           
+            <input placeholder='Verification code' />
+          </Form.Field>
+        
+          <Button type='submit' fluid primary>
+           Next
+          </Button>
+        </Form>
+        <div className='backToLogin'>
+          <Link to='/login'>Resend Code</Link>
+        </div>
+      </Container>
     </Auth>
   );
 };
