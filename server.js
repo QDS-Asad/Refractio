c1b3064cc161c3b9a785b2e022ad277272d647a4
QDS-Rakesh <qdsdev-in01@quodisys.com>
2022-05-31 09:04:42 +0500

    front end path updated

--- server.js
+++ server.js
@@ -23,10 +23,10 @@ app.get('/api/status', (req, res) => {
 
 app.use('/api/users', users);
 
-app.use(express.static(path.join(__dirname, '/client/build')));
+app.use('/', express.static(path.join(__dirname, '/client/build')));
 
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
 });
 
 const PORT = process.env.PORT || 4001;
