3d5dcf052afe8e111ba875f522db5f68165dbef7
QDS-Ravi <qdsdev-in06@quodisys.com>
2022-06-02 17:04:23 +0530

    restructue user routes

--- server/src/routes/user.routes.js
+++ server/src/routes/user.routes.js
@@ -10,15 +10,15 @@ const verifyToken = require('../middlewares/verifyToken');
 
 const metaData = require('../models/projectmetadata').MetaData;
 
-router.post('/api/register',generalRegister,UserController.register);
-router.post('/api/login',UserController.login);
-router.post('/api/verify-login',UserController.verifyOtp);
-router.post('/api/forget-password',UserController.forgetPassword);
-router.post('/api/reset-password/:token',generalResetPassword,UserController.resetPassword)
+router.post('/register',generalRegister,UserController.register);
+router.post('/login',UserController.login);
+router.post('/verify-login',UserController.verifyOtp);
+router.post('/forget-password',UserController.forgetPassword);
+router.post('/reset-password/:token',generalResetPassword,UserController.resetPassword)
 //router.get('/api/test',verifyToken,(req, res)=>{res.send({message:"Token verfied successfully."})})
 
 //seeding project meta-data
-router.post('/api/meta-data',async(req, res)=>{
+router.post('/meta-data',async(req, res)=>{
     let projectMetaData = await metaData.create({
         roles:[
             {
