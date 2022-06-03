a0e5ecddaa2e7429b705cd11bb86c3527b1df41d
QDS-Rakesh <qdsdev-in01@quodisys.com>
2022-06-03 08:27:01 +0500

    removed unused code

--- server/src/server.js
+++ server/src/server.js
@@ -1,22 +1,21 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const bodyParser = require('body-parser');
+// const express = require('express');
+// const dotenv = require('dotenv');
+// const bodyParser = require('body-parser');
 
-const users = require('./routes/user.routes');
+// const users = require('./routes/user.routes');
 
-dotenv.config();
-const db = require('./config/db');
+// dotenv.config();
+// const db = require('./config/db');
 
-const app = express();
+// const app = express();
 
-app.use(bodyParser.urlencoded({extended: false}));
+// app.use(bodyParser.urlencoded({extended: false}));
 
-// parse requests of content-type - application/json
-app.use(bodyParser.json());
+// // parse requests of content-type - application/json
+// app.use(bodyParser.json());
 
-app.use('/', users);
+// app.use('/', users);
 
+// const PORT = process.env.PORT || 8000;
 
-const PORT = process.env.PORT || 8000; 
-
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+// app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
