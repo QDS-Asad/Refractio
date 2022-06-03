362a4654d8e3dc728b59f9d9c54c593b50951cfd
QDS-Rakesh <qdsdev-in01@quodisys.com>
2022-06-03 07:40:06 +0500

    removed middlewares

--- server/src/routes/user.routes.js
+++ server/src/routes/user.routes.js
@@ -1,46 +1,46 @@
 const express = require('express');
 const router = express.Router();
-const generalRegister = require('../middlewares/generalRegister').validateGeneralUserRegisterInput;
-const generalLogin = require('../middlewares/generalLogin').validateGeneralUserLoginInput;
-const generalResetPassword = require('../middlewares/generalResetPassword').validateGeneralUserResetPasswordInput;
+const generalRegister =
+  require('../middlewares/generalRegister').validateGeneralUserRegisterInput;
+const generalLogin =
+  require('../middlewares/generalLogin').validateGeneralUserLoginInput;
+const generalResetPassword =
+  require('../middlewares/generalResetPassword').validateGeneralUserResetPasswordInput;
 
-
-const UserController = require('../controllers/user.controller')
+const UserController = require('../controllers/user.controller');
 const verifyToken = require('../middlewares/verifyToken');
 
 const metaData = require('../models/projectmetadata').MetaData;
 
-router.post('/register',generalRegister,UserController.register);
-router.post('/login',UserController.login);
-router.post('/verify-login',UserController.verifyOtp);
-router.post('/forget-password',UserController.forgetPassword);
-router.post('/reset-password/:token',generalResetPassword,UserController.resetPassword)
+router.post('/register', UserController.register);
+router.post('/login', UserController.login);
+router.post('/verify-login', UserController.verifyOtp);
+router.post('/forget-password', UserController.forgetPassword);
+router.post('/reset-password/:token', UserController.resetPassword);
 //router.get('/api/test',verifyToken,(req, res)=>{res.send({message:"Token verfied successfully."})})
 
 //seeding project meta-data
-router.post('/meta-data',async(req, res)=>{
-    let projectMetaData = await metaData.create({
-        roles:[
-            {
-                role:"Participant",
-                key:"participant",
-            },
-            {
-                role:"Organiser",
-                key:"organiser"
-            },
-            {
-                role:"Administrator",
-                key:"administrator"   
-            }
-        ]
-    });
-    res.status(200).send({
-        status:"Success",
-       projectMetaData
-    })
-})
-
-
+router.post('/meta-data', async (req, res) => {
+  let projectMetaData = await metaData.create({
+    roles: [
+      {
+        role: 'Participant',
+        key: 'participant',
+      },
+      {
+        role: 'Organiser',
+        key: 'organiser',
+      },
+      {
+        role: 'Administrator',
+        key: 'administrator',
+      },
+    ],
+  });
+  res.status(200).send({
+    status: 'Success',
+    projectMetaData,
+  });
+});
 
 module.exports = router;
