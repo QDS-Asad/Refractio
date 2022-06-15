475a72c361470094df0e9b0366b316f61bb3d032
QDS-Rakesh <qdsdev-in01@quodisys.com>
2022-06-15 10:32:38 +0500

    minor-issue

--- server.js
+++ server.js
@@ -26,21 +26,22 @@ app.use(express.json());
 app.use(bodyParser.json());
 app.use(cors());
 
-app.all('/*', function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', constants.CLIENT_HOST);
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Content-Type, Authorization, Content-Length, X-Requested-With'
-  );
-  res.header('Access-Control-Allow-Credentials', true);
-  next();
-});
+// app.all('/*', function (req, res, next) {
+//   res.header('Access-Control-Allow-Origin', constants.CLIENT_HOST);
+//   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+//   res.header(
+//     'Access-Control-Allow-Headers',
+//     'Content-Type, Authorization, Content-Length, X-Requested-With'
+//   );
+//   res.header('Access-Control-Allow-Credentials', true);
+//   next();
+// });
+
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swagger));
+
 app.get('/api/status', (req, res) => {
   res.json({ message: 'Api is working.' });
 });
-
-app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swagger));
 
 app.use('/api/users', users);
 app.use('/api/roles', roles);
