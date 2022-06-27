ff6a6f7304433249fb40bb685898b1eb1ff2f1bd
Harry Ahmad <haroon.ahmad.cs@gmail.com>
2022-06-27 15:09:21 +0500

    changes

--- server/src/helpers/encryption_helper.js
+++ server/src/helpers/encryption_helper.js
@@ -3,16 +3,23 @@ const { CRYPTO_KEY } = require('../lib/constants');
 
 const crypto_encrypt = function (originalText) {
   const ciphertext = CryptoJS.AES.encrypt(originalText, CRYPTO_KEY).toString();
-  console.log(ciphertext, encodeURI(ciphertext));
-  return encodeURI(ciphertext);
+  console.log(ciphertext, encodeUrl(ciphertext));
+  return encodeUrl(ciphertext);
 }
 
 const crypto_decrypt = function (ciphertext) {
-  let bytes = CryptoJS.AES.decrypt(decodeURI(ciphertext), CRYPTO_KEY);
+  let bytes = CryptoJS.AES.decrypt(decodeUrl(ciphertext), CRYPTO_KEY);
   let originalText = bytes.toString(CryptoJS.enc.Utf8);
   return originalText;
 }
 
+const encodeUrl = (text) => {
+  return text.toString().replace('+','xMl3Jk').replace('/','Por21Ld').replace('=','Ml32');
+}
+
+const decodeUrl = (text) => {
+  return text.toString().replace('xMl3Jk', '+' ).replace('Por21Ld', '/').replace('Ml32', '=');
+}
 
 module.exports = {
   crypto_encrypt: crypto_encrypt,
