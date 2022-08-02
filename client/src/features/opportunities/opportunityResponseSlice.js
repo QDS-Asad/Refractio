c79efe259cb67a6480dfc128cc9a7205ca4baf74
usamaahmad123 <usama.ahmad@phenologix.com>
2022-08-02 08:52:22 +0500

    slice bugs

--- client/src/features/opportunities/opportunityResponseSlice.js
+++ client/src/features/opportunities/opportunityResponseSlice.js
@@ -1,12 +1,12 @@
 import { createSlice } from '@reduxjs/toolkit';
 import refractioApi from '../../common/refractioApi';
+import { getResponse } from './opportunityGetResponseSlice';
 
 // initial state
 export const initialState = {
   loading: false,
   error: null,
   opportunity: null,
-  response: null,
   success: false,
   message: '',
 };
@@ -25,11 +25,6 @@ const opportunityResponseSlice = createSlice({
       state.error = false;
       state.opportunity = payload;
     },
-    setResponse: (state, { payload }) => {
-      state.loading = false;
-      state.error = false;
-      state.response = payload;
-    },
     setError: (state, { payload }) => {
       state.loading = false;
       state.error = payload;
@@ -46,7 +41,6 @@ const opportunityResponseSlice = createSlice({
       state.opportunity = null;
       state.message = '';
       state.success = false;
-      state.response = null;
     },
   },
 });
@@ -56,7 +50,6 @@ export const {
   setOpportunity,
   setError,
   setSuccess,
-  setResponse,
   reset,
 } = opportunityResponseSlice.actions;
 
@@ -92,21 +85,6 @@ export const respondOpportunity = (id, body) => async (dispatch) => {
       dispatch(setSuccess('Response published successfully.'));
     }
     dispatch(getResponse(id));
-  } catch (error) {
-    const errorMessage =
-      error.response && error.response.data
-        ? error.response.data.message
-        : error.message;
-    dispatch(setError(errorMessage));
-  }
-};
-export const getResponse = (id) => async (dispatch) => {
-  try {
-    dispatch(setLoading());
-    let { data: response } = await refractioApi.get(
-      `/opportunities/opportunity-response/${id}`
-    );
-    dispatch(setResponse(response.data));
   } catch (error) {
     const errorMessage =
       error.response && error.response.data
