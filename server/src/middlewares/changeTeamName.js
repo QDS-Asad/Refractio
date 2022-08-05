4e776ad2ee605d7d2291ae277fd6aac28fe4ada7
Harry Ahmad <haroon.ahmad.cs@gmail.com>
2022-08-05 12:57:52 +0500

    changes

---
+++ server/src/middlewares/changeTeamName.js
@@ -0,0 +1,20 @@
+const { check, validationResult } = require('express-validator');
+const { errorResp } = require("../helpers/error_helper");
+const { HTTP_STATUS, ERROR_MESSAGE, REGEX_PATTERN } = require("../lib/constants");
+
+module.exports. validateChangeTeamName = [
+    check('name')
+    .exists().bail().withMessage(ERROR_MESSAGE.REQUIRED)
+    .notEmpty().bail().withMessage(ERROR_MESSAGE.NOT_EMPTY),
+    (req, res, next) => {
+        try {
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+                return errorResp(res, { code: HTTP_STATUS.BAD_REQUEST.CODE, msg: errors.array()})
+            }
+            next();
+        } catch (err) {
+            res.status(HTTP_STATUS.INTERNAL_SERVER.CODE).json(errorResp());
+        }
+    }
+]
