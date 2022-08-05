42821cc1949ec2f2bcedd2fdafe509aced48bf3f
usamaahmad123 <usama.ahmad@phenologix.com>
2022-08-05 11:33:17 +0500

    responsive app layout

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,9 +1,10 @@
 import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
-import { Dropdown, Header, Menu } from 'semantic-ui-react';
+import { Dropdown, Header, Icon, Menu } from 'semantic-ui-react';
+import MediaQuery from 'react-responsive';
 import { authLoginSelector } from '../features/auth/authLoginSlice';
 import EditProfile from './EditProfile';
-const NavBar = ({ showLogo = false }) => {
+const NavBar = ({ showLogo = false, setVisible }) => {
   const [editProfile, setEditProfile] = useState(false);
   const { userLogin } = useSelector(authLoginSelector);
   return (
@@ -22,14 +23,21 @@ const NavBar = ({ showLogo = false }) => {
             </Menu.Item>
           </Menu.Menu>
         )}
-
+        {setVisible && (
+          <MediaQuery maxWidth={800}>
+            <Menu.Menu position='left'>
+              <Menu.Item onClick={() => setVisible((prev) => !prev)} header>
+                <Icon name='content' size='large' />
+              </Menu.Item>
+            </Menu.Menu>
+          </MediaQuery>
+        )}
         <Menu.Menu position='right'>
           <Dropdown
             item
             text={`${userLogin.firstName} ${userLogin.lastName} `}
             className='me-4'
-            style={{ color: '#98A6AD', minHeight: '4.5rem' }}
-          >
+            style={{ color: '#98A6AD', minHeight: '4.5rem' }}>
             <Dropdown.Menu>
               <Dropdown.Item
                 icon='edit'
