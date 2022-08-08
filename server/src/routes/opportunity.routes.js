9cc3073ee718a68c07eb79ac50557ddaa4657e62
Harry Ahmad <haroon.ahmad.cs@gmail.com>
2022-08-08 15:42:11 +0500

    changes

--- server/src/routes/opportunity.routes.js
+++ server/src/routes/opportunity.routes.js
@@ -71,85 +71,85 @@ const { validateUpdateOpportunity } = require("../middlewares/updateOpportunity"
 router.get("/", Auth, Opportunity.opportunitiesList);
 
 /**
-//  * @swagger
-//  *   /api/opportunities/all:
-//  *   get:
-//  *     description: get all users opportunities list
-//  *     tags: [Opportunities]
-//  *     parameters:
-//  *        - in: query
-//  *          name: page
-//  *          schema:
-//  *              type: integer
-//  *        - in: query
-//  *          name: page_size
-//  *          schema:
-//  *              type: integer
-//  *     responses:
-//  *        '200':
-//  *           description: Success
-//  *           content:
-//  *             application/json:
-//  *               schema:
-//  *                 type: object
-//  *                 properties:
-//  *                   success:
-//  *                     type: string
-//  *                   code:
-//  *                     type: integer
-//  *                   message:
-//  *                     type: string
-//  *                   data:
-//  *                     type: object
-//  *                 example:
-//  *                   success: true
-//  *                   code: 200
-//  *                   message: Operation successfull.
-//  *        '404':
-//  *           description: Operation Failed
-//  *           content:
-//  *             application/json:
-//  *               schema:
-//  *                 type: object
-//  *                 properties:
-//  *                   success:
-//  *                     type: string
-//  *                   code:
-//  *                     type: integer
-//  *                   message:
-//  *                     type: string
-//  *                   data:
-//  *                     type: object
-//  *                 example:
-//  *                    {"success": false,"code": 404,"message": "Operation Failed."}
-//  *
-//  *        '422':
-//  *           description: Unprocessable entity - This occurs in cases where data might not be valid (E.g Data provided is not valid.)
-//  *           content:
-//  *             application/json:
-//  *               schema:
-//  *                 type: object
-//  *                 properties:
-//  *                   success:
-//  *                     type: string
-//  *                   code:
-//  *                     type: integer
-//  *                   message:
-//  *                     type: string
-//  *                   data:
-//  *                     type: object
-//  *                 example:
-//  *                    {"success": false,"code": 422,"message": "Data provided is not valid."}
-//  * components:
-//  *  schemas:
-//  *   AllOpportunity:
-//  *    type: object
-//  *    properties:
-//  *      page:
-//  *        type: number
-//  *      page_size:
-//  *        type: number
-//  */
+ * @swagger
+ *   /api/opportunities/all:
+ *   get:
+ *     description: get all users opportunities list
+ *     tags: [Opportunities]
+ *     parameters:
+ *        - in: query
+ *          name: page
+ *          schema:
+ *              type: integer
+ *        - in: query
+ *          name: page_size
+ *          schema:
+ *              type: integer
+ *     responses:
+ *        '200':
+ *           description: Success
+ *           content:
+ *             application/json:
+ *               schema:
+ *                 type: object
+ *                 properties:
+ *                   success:
+ *                     type: string
+ *                   code:
+ *                     type: integer
+ *                   message:
+ *                     type: string
+ *                   data:
+ *                     type: object
+ *                 example:
+ *                   success: true
+ *                   code: 200
+ *                   message: Operation successfull.
+ *        '404':
+ *           description: Operation Failed
+ *           content:
+ *             application/json:
+ *               schema:
+ *                 type: object
+ *                 properties:
+ *                   success:
+ *                     type: string
+ *                   code:
+ *                     type: integer
+ *                   message:
+ *                     type: string
+ *                   data:
+ *                     type: object
+ *                 example:
+ *                    {"success": false,"code": 404,"message": "Operation Failed."}
+ *
+ *        '422':
+ *           description: Unprocessable entity - This occurs in cases where data might not be valid (E.g Data provided is not valid.)
+ *           content:
+ *             application/json:
+ *               schema:
+ *                 type: object
+ *                 properties:
+ *                   success:
+ *                     type: string
+ *                   code:
+ *                     type: integer
+ *                   message:
+ *                     type: string
+ *                   data:
+ *                     type: object
+ *                 example:
+ *                    {"success": false,"code": 422,"message": "Data provided is not valid."}
+ * components:
+ *  schemas:
+ *   AllOpportunity:
+ *    type: object
+ *    properties:
+ *      page:
+ *        type: number
+ *      page_size:
+ *        type: number
+ */
 router.get("/all", Auth, Opportunity.getAllOpportunities);
 
 /**
